e51b437c6626d501f62cbd7865f3e8a103574bd6
EnnieAyo <ayoennie@gmail.com>
2024-11-01 10:29:10 -0400

    added async/wait and promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,37 +25,44 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    //Write your code here
-    let allBooks = JSON.stringify(books)
+public_users.get('/', async function (req, res) {
+    let allBooks = await JSON.stringify(books)
     return res.send(allBooks);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    //Write your code here
     let isbn = req.params.isbn;
     if (isbn) {
         let foundBook = books[isbn]
-        return res.send(foundBook);
+
+        let myPromise1 = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(foundBook)
+            }, 5000)
+        });
+
+        myPromise1.then((data) => { 
+            return res.send(data) 
+        })
     }
+    ;
     //   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-    //Write your code here
+public_users.get('/author/:author', async function (req, res) {
     let author = req.params.author;
     let found = [];
     if (author) {
         let bookKeys = Object.keys(books);
-        bookKeys.forEach((bookkKey) => {
+        await bookKeys.forEach((bookkKey) => {
             if (author === books[bookkKey].author) {
                 found.push(books[bookkKey])
             }
         });
         if (found[0]) {
-            return res.status(404).json(found);
+            return res.status(200).json(found);
         } else {
             return res.status(404).json({ message: "author not found" })
         }
@@ -65,19 +72,19 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     //Write your code here
     let title = req.params.title;
     let found = [];
     if (title) {
         let bookKeys = Object.keys(books);
-        bookKeys.forEach((bookkKey) => {
+        await bookKeys.forEach((bookkKey) => {
             if (title === books[bookkKey].title) {
                 found.push(books[bookkKey])
             }
         });
         if (found[0]) {
-            return res.status(404).json(found);
+            return res.status(200).json(found);
         } else {
             return res.status(404).json({ message: "title not found" })
         }
